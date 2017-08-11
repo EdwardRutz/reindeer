a2225fdf8e62e02c5afb961910f38be86a14b7ab
Edward R <edwardrz@users.noreply.github.com>
2017-08-11 17:50:26 -0500

    updated

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 console.log("main.js active");
+/*
+[_] Place output in circles
+[_] Fix: first reindeer doesn't have a color
+*/
 
 /***********  SCRIPT  ***************/
-var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var colors = ["Blue ", "Red ", "Orange ", "Purple ", "Hazel ", "Aquamarine ", "Periwinkle ", "Azure ", "Fuchsia ", "Chocolate ", "Amber ", "Amaranth "];
 var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
 
+var hohohoElement = document.getElementById("output1");
+	console.log("hohohoElement", hohohoElement);
 
-var hohohoElement = document.getElementById("reindeer");
+for(i = 0; i < reindeer.length; i+=1){
+	hohohoElement.innerHTML += `<li>${colors[i].concat(reindeer[i])}</li>`;
+};
 
-
-
-
+console.log("hohohoElement", hohohoElement);
+console.log("output1", output1);
